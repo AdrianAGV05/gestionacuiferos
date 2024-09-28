e67d548c74a35dc042067bf3a7e0dc9bce46933f
AdrianAGV05 <adrian55abrham@gmail.com>
2024-09-27 21:12:34 -0600

    comit inicial

---
+++ js/script.js
@@ -0,0 +1,45 @@
+let currentSlide = 0;
+
+function showSlide(index) {
+    const slides = document.querySelectorAll('.carousel-images img');
+    const totalSlides = slides.length;
+
+    if (index >= totalSlides) {
+        currentSlide = 0;
+    } else if (index < 0) {
+        currentSlide = totalSlides - 1;
+    } else {
+        currentSlide = index;
+    }
+
+    const offset = -currentSlide * 100;
+    
+    if(document.querySelector('.carousel-images')!=null){
+      document.querySelector('.carousel-images').style.transform = `translateX(${offset}%)`;
+    }
+    
+}
+
+function moveSlide(step) {
+    showSlide(currentSlide + step);
+}
+
+// Initialize
+showSlide(currentSlide);
+
+// Optional: Auto-slide
+setInterval(() => moveSlide(1), 5000);
+
+function saludos(){
+  Swal.fire({
+    title:'Datos enviados y en espera, la confirmación puede tardar entre uno y tres días hábiles'
+  })
+}
+
+function hola(){
+  Swal.fire({
+    title:'Datos enviados para almacenamiento',
+    icon:'success',
+    timer:1500
+  })
+}
