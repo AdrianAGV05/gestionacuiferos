2a69b7864ba7dd1aced947f30ea0e271d1ba61bd
Andrés Gallego <agallegol2002@gmail.com>
2026-09-22 12:44:26 -0600

    Corregir promp anterior para evitar duplicacion de /api en los fetch

--- frontend/js/script.js
+++ frontend/js/script.js
@@ -24,7 +24,7 @@ function iniciarSesion() {
 
   const credenciales = { correo, contrasena };
 
-  fetch(`${API_BASE_URL}usuarios/login`, {
+  fetch(`${API_BASE_URL}/usuarios/login`, {
     method: 'POST',
     headers: { 'Content-Type': 'application/json' },
     body: JSON.stringify(credenciales)
